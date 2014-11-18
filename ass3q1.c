1a76a308de5d9c6b3865d464d89732d40f8d12f3
William ST-ARNAUD <wstarn@teaching.CS.McGill.CA>
2014-11-17 23:08:17 -0500

    a.out file

--- ass3q1.c
+++ ass3q1.c
@@ -1,10 +1,7 @@
 #include<stdio.h>
-#include<stdlib.h>
 
-FILE *file= system("'$1'");
+FILE *file= system("`$1`");
 
-FILE *fp= fopen("'file'", "r");
+FILE *fp= fopen(file, 100);
 
-void main (void) {
-	printf(fp);
-}
+
